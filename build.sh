2f6dc0b43f98317152fe1dfd93de6ada9446928c
Aeonix <aeoniixx@gmail.com>
2024-12-03 13:36:56 +0530

    build.sh: Update

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-rm -rf .repo/local_manifests
+rm -rf .repo/local_manifests;
 
-repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs
-git clone https://github.com/Aeoniixx-Lavender/local_manifests -b TPP-14 .repo/local_manifests
-/opt/crave/resync.sh
+repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs;
+git clone https://github.com/Aeoniixx-Lavender/local_manifests -b TPP-14 .repo/local_manifests;
+git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template.git vendor/tpp-priv/keys;
 
-export BUILD_USERNAME=Aeonix 
-export BUILD_HOSTNAME=Crave
+/opt/crave/resync.sh;
 
-. build/envsetup.sh
-lunch aosp_lavender-userdebug 
+cd vendor/tpp-priv/keys/;
+bash keys.sh;
+cd /tmp/src/android;
+
+export BUILD_USERNAME=Aeonix;
+export BUILD_HOSTNAME=Crave;
+
+source build/envsetup.sh;
+lunch aosp_lavender-userdebug;
 make bacon -j$(nproc --all)
