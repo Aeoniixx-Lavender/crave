cb31507b544d9140cccc8106e7b0bc63c363c2e9
Aeonix <138367906+Aeoniixx@users.noreply.github.com>
2024-12-11 09:50:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests;
-
-repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3 --git-lfs;
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
-
-git clone https://github.com/Aeoniixx-Lavender/local_manifests -b PixelStar-14 .repo/local_manifests;
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
 
 export BUILD_USERNAME=Aeonix;
 
 source build/envsetup.sh;
-make clean;
-lunch pixelstar_lavender-userdebug;
-make bacon -j$(nproc --all)
+lunch pixelstar_lavender-user;
+make bacon
