eefa710f4edded43598ec583a0bb49faa8ccb98f
Aeonix <138367906+Aeoniixx@users.noreply.github.com>
2024-12-11 09:13:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,5 +11,6 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
 export BUILD_USERNAME=Aeonix;
 
 source build/envsetup.sh;
+make clean;
 lunch pixelstar_lavender-userdebug;
 make bacon -j$(nproc --all)
