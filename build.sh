f7b3b7373e3e0542983c347abc3b8f797b073848
Aeoniixx <138367906+Aeoniixx@users.noreply.github.com>
2025-03-03 14:20:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16;
 
-export BUILD_USERNAME=Aeoniixx;
-export BUILD_HOSTNAME=Aeoniixx;
-
 source build/envsetup.sh;
-lunch aosp_lavender-user;
+lunch aosp_lavender-ap4a-userdebug;
 make bacon
