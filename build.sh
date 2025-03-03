b949c4dc88e8503e15a82c3df88ff06352f9dc84
Aeoniixx <138367906+Aeoniixx@users.noreply.github.com>
2025-03-04 04:55:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16;
 
+rm -rf packages/apps/Aperture;
+git clone https://github.com/Aeoniixx/android_packages_apps_Aperture -b lineage-22.1 packages/apps/Aperture;
+
 source build/envsetup.sh;
 lunch aosp_lavender-ap4a-userdebug;
 make bacon
