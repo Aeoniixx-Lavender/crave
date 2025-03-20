e051c5a8ca7b36272595706db582423efdb5a3b7
Aeoniixx <138367906+Aeoniixx@users.noreply.github.com>
2025-03-20 14:05:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16;
+#repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16;
 
 rm -rf packages/apps/Aperture;
 git clone https://github.com/Aeoniixx/android_packages_apps_Aperture -b lineage-22.1 packages/apps/Aperture;
